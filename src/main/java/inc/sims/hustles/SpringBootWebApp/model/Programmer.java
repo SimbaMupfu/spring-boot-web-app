ddaeec05b53ad8efa6986f51ec1f9f91b30f2e82
SimbaMupfu <scmupfururirwa@gmail.com>
2025-08-20 14:59:20 +0200

    Create Programmer model

---
+++ src/main/java/inc/sims/hustles/SpringBootWebApp/model/Programmer.java
@@ -0,0 +1,22 @@
+package inc.sims.hustles.SpringBootWebApp.model;
+
+public class Programmer {
+    private int programmerId;
+    private String programmerName;
+
+    public int getProgrammerId() {
+        return programmerId;
+    }
+
+    public void setProgrammerId(int programmerId) {
+        this.programmerId = programmerId;
+    }
+
+    public String getProgrammerName() {
+        return programmerName;
+    }
+
+    public void setProgrammerName(String programmerName) {
+        this.programmerName = programmerName;
+    }
+}
