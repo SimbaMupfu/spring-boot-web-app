515200a3d789639cf7f8f31b6102ac0811c3b968
SimbaMupfu <scmupfururirwa@gmail.com>
2025-08-20 13:28:42 +0200

    Use model instead of session.

--- src/main/java/inc/sims/hustles/SpringBootWebApp/controller/HomeController.java
+++ src/main/java/inc/sims/hustles/SpringBootWebApp/controller/HomeController.java
@@ -3,6 +3,7 @@ package inc.sims.hustles.SpringBootWebApp.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,10 +16,10 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int firstNumber, int num2, HttpSession session){
+    public String add(@RequestParam("num1") int firstNumber, int num2, Model model){
         int result = firstNumber + num2;
 
-        session.setAttribute("result", result);
+        model.addAttribute("result", result);
         return "result.jsp";
     }
 }
